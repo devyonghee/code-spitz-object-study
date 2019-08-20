d61c689fbdd7c15dcd69b15387dc9bf0258ce230
devyonghee <devyonghee@gmail.com>
2019-08-21 08:29:03 +0900

    original

--- src/main/java/theater/Main.java
+++ src/main/java/theater/Main.java
@@ -1,12 +1,6 @@
 package theater;
 
 public class Main {
-    // 마지막 theater예제에서 Theater는 단 하나의 영화만 고정가격으로 상영 중이다.
-    // 다양한 가격의 영화를 상영할 수 있게 개선하라. (Movie클래스가 새롭게 필요하고 또한 이에 따라 초대, 티켓, 티켓 오피스등의 총괄적인 변화가 일어남)
-    // 무료 상영 영화
-    // 관람객이 다양한 티켓을 가질 수 있도록
-    // 초대권, 특정 영화만 가능하도록 변경
-    // 티켓, 특정 영화에 종속
     public static void main(String[] args) {
         Theater theater = new Theater(100L);
         Audience audience1 = new Audience(0L);
